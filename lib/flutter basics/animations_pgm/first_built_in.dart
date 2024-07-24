2b08bc5e2640a7f0b5d05277526f4dc7e17a160a
ajithbabu <ajithpersonaluse@gmail.com>
2024-07-24 23:18:16 +0530

    training demo

---
+++ lib/flutter basics/animations_pgm/first_built_in.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Myanimationsfirst(),
+  ));
+}
+
+class Myanimationsfirst extends StatefulWidget {
+  const Myanimationsfirst({super.key});
+
+  @override
+  State<Myanimationsfirst> createState() => _MyanimationsfirstState();
+}
+
+class _MyanimationsfirstState extends State<Myanimationsfirst> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: LoadingAnimationWidget.horizontalRotatingDots(color: Colors.red, size: 100),
+        // child: LoadingAnimationWidget.bouncingBall(color: Colors.yellow, size: 100)
+        ),
+    );
+  }
+}
